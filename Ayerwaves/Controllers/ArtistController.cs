3d496dea2e3923f7ff0f80afd61b4ff02ad8dece
Leo Taylor <leotaylor723@gmail.com>
2019-01-26 12:08:14 -0600

    get all artists

--- Ayerwaves/Controllers/ArtistController.cs
+++ Ayerwaves/Controllers/ArtistController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Ayerwaves.DBAccess;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace Ayerwaves.Controllers
 {
@@ -11,5 +13,18 @@ namespace Ayerwaves.Controllers
     [ApiController]
     public class ArtistController : ControllerBase
     {
+        private readonly ArtistStorage _storage;
+
+        public ArtistController(IConfiguration config)
+        {
+            _storage = new ArtistStorage(config);
+        }
+
+        [HttpGet("artists")]
+        public IActionResult GetAllArtists()
+        {
+            var result = _storage.GetAll();
+            return Ok(result);
+        }
     }
 }
--- Ayerwaves/DBAccess/ArtistStorage.cs
+++ Ayerwaves/DBAccess/ArtistStorage.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Ayerwaves.Models;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +11,23 @@ namespace Ayerwaves.DBAccess
 {
     public class ArtistStorage
     {
+        private string ConnectionString;
+
+        public ArtistStorage(IConfiguration config)
+        {
+            ConnectionString = config.GetSection("ConnectionString").Value;
+        }
+
+        public List<Artist> GetAll()
+        {
+            using(var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                var result = connection.Query<Artist>(@"Select * FROM Artist");
+
+                return result.ToList();
+            }
+        }
     }
 }
