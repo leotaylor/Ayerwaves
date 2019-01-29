bb6559d5f352707e4d8d6a01442f99fcd06d07af
Leo Taylor <leotaylor723@gmail.com>
2019-01-28 19:35:53 -0600

    added vt storage and model

---
+++ Ayerwaves/DBAccess/VendorTypeStorage.cs
@@ -0,0 +1,87 @@
+﻿using Ayerwaves.Models;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ayerwaves.DBAccess
+{
+    public class VendorTypeStorage
+    {
+        private string ConnectionString;
+
+        public VendorTypeStorage(IConfiguration config)
+        {
+            ConnectionString = config.GetSection("ConnectionString").Value;
+        }
+
+        public List<VendorType> GetAll()
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                var result = connection.Query<VendorType>(@"Select * from VendorType");
+
+                return result.ToList();
+            }
+        }
+
+        public VendorType GetById(int Id)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                var result = connection.QueryFirst<VendorType>(@"select *
+                                                                from VendorType
+                                                                where id = @Id", new { Id });
+                return result;
+            }
+        }
+
+        public bool Add(VendorType vendorType)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                var result = connection.Execute(@"INSERT into dbo.VendorType (VendorType)
+                                                    Values (@TypeOfVendor )", vendorType);
+
+                return result == 1;
+            }
+        }
+
+        public bool UpdateVendorType(int id, VendorType vendorType)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                vendorType.id = id;
+
+                var result = connection.Execute(@"UPDATE VendorType
+                                                    SET [VendorType] = @TypeOfVendor
+                                                    WHERE id = @id", vendorType);
+                return result == 1;
+            }
+        }
+
+        public bool DeleteVendorType(int Id)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                var result = connection.Execute(@"DELETE FROM VendorType
+                                                    WHERE id = @id", new { id = Id });
+
+                return result == 1;
+            }
+        }
+    }
+}
---
+++ Ayerwaves/Models/VendorType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ayerwaves.Models
+{
+    public class VendorType
+    {
+        public int id { get; set; }
+        public string TypeOfVendor { get; set; }
+    }
+}
